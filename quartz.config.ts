23e16f923a80b7f46cc9ecdefb1c3b0ea473b4cd
Nikita Lanskov <leins275@gmail.com>
2024-06-01 19:23:01 +0300

    fix: Updated page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "leins275 digital garden",
+    pageTitle: "leins275 thoughts",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
