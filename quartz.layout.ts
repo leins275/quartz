4fbf277566d0b551925c8c48bf2a323a76c3e4d3
leins275 <leins275@gmail.com>
2024-06-02 14:52:06 +0300

    Quartz sync: Jun 2, 2024, 2:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,10 +30,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Graph(),
     //Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
