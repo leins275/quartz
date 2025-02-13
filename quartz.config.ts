4699de6d2e93b554bdaf60e70ac40902bd5f4fa7
leins275 <leins275@gmail.com>
2025-02-13 18:44:01 +0300

    feat(style): added font with css

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "iAWriterMonoS-Regular", //"Source Sans Pro",
+        body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
