729f8878798c1bc823dbe26d54204f38d925e605
leins275 <leins275@gmail.com>
2025-08-14 21:38:53 +0300

    feat: added Commets from official instruction

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+      Component.Comments({
+        provider: 'giscus',
+        options: {
+          // from data-repo
+          repo: 'leins275/quartz',
+          // from data-repo-id
+          repoId: 'R_kgDOMDhNMQ',
+          // from data-category
+          category: 'Announcements',
+          // from data-category-id
+          categoryId: 'DIC_kwDOMDhNMc4CuL7L',
+          // from data-lang
+          lang: 'ru'
+        }
+      }),
+  ],
   footer: Component.Footer({
     links: {
       "Hugo blog": "https://blog.leins275.xyz",
